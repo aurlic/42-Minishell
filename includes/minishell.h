45d50584d54a63efdeebeb3877c36125f6587d6c
Thibrac <thibrac@gmail.com>
2024-02-06 10:58:10 +0100

    .h file added

---
+++ includes/minishell.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: traccurt <traccurt@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/05 17:39:53 by traccurt          #+#    #+#             */
+/*   Updated: 2024/02/05 17:41:49 by traccurt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+#define MINISHELL_H
+
+# include <errno.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <unistd.h>
+# include "../srcs/libft/libft.h"
+
+#endif
