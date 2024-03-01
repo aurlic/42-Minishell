71215d9f89ae1a27029d19be1d5c6ff0d71aec19
Arthur Urlic <aurlic@student.42.fr>
2024-03-01 11:11:00 +0100

    easter egg

--- srcs/main/prompt.c
+++ srcs/main/prompt.c
@@ -6,7 +6,7 @@
 /*   By: aurlic <aurlic@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 18:19:35 by aurlic            #+#    #+#             */
-/*   Updated: 2024/02/28 16:42:25 by aurlic           ###   ########.fr       */
+/*   Updated: 2024/03/01 10:51:51 by aurlic           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,14 @@ char	*read_prompt(t_shell *shell)
 	str = readline(prompt);
 	if (ft_strictcmp(str, "xxx"))
 		prompt = PROMPT_X;
+	else if (ft_strictcmp(str, "kiwi"))
+		prompt = PROMPT_K;
+	else if (ft_strictcmp(str, "celine"))
+		prompt = PROMPT_D;
+	else if (ft_strictcmp(str, "jackson"))
+		prompt = PROMPT_J;
+	else if (ft_strictcmp(str, "voyage"))
+		prompt = PROMPT_R;
 	else if (ft_strictcmp(str, "jeanmi"))
 		prompt = PROMPT;
 	if (!str)
