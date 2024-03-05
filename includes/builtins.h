59057d03dcec3dc2a1c6a6e5c6b32e5c90bc5dd3
traccurt <traccurt@student.42.fr>
2024-03-05 18:01:04 +0100

    working on builtins, and redir

--- includes/builtins.h
+++ includes/builtins.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtins.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aurlic <aurlic@student.42.fr>              +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 11:17:43 by aurlic            #+#    #+#             */
-/*   Updated: 2024/02/06 11:19:29 by aurlic           ###   ########.fr       */
+/*   Updated: 2024/03/05 17:45:13 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,13 @@
 # include "../srcs/libft/includes/libft.h"
 # include "../srcs/libft/includes/ft_printf.h"
 # include "../srcs/libft/includes/get_next_line.h"
+
+# define ECHO 2001
+# define CD 2002
+# define PWD 2003
+# define EXPORT 2004
+# define UNSET 2005
+# define ENV 2006
+# define EXIT 2007
 
 #endif
--- srcs/builtins/builtins.c
+++ srcs/builtins/builtins.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtins.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aurlic <aurlic@student.42.fr>              +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 15:36:50 by aurlic            #+#    #+#             */
-/*   Updated: 2024/03/01 15:37:20 by aurlic           ###   ########.fr       */
+/*   Updated: 2024/03/05 17:44:55 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,19 @@
 
 int	is_builtin(char *str)
 {
-	if (ft_strictcmp(str, "echo") || ft_strictcmp(str, "\'echo\'")
-		|| ft_strictcmp(str, "\"echo\""))
-		return (1);
-	if (ft_strictcmp(str, "cd") || ft_strictcmp(str, "\'cd\'")
-		|| ft_strictcmp(str, "\"cd\""))
-		return (1);
-	if (ft_strictcmp(str, "pwd") || ft_strictcmp(str, "\'pwd\'")
-		|| ft_strictcmp(str, "\"pwd\""))
-		return (1);
-	if (ft_strictcmp(str, "export") || ft_strictcmp(str, "\'export\'")
-		|| ft_strictcmp(str, "\"export\""))
-		return (1);
-	if (ft_strictcmp(str, "unset") || ft_strictcmp(str, "\'unset\'")
-		|| ft_strictcmp(str, "\"unset\""))
-		return (1);
-	if (ft_strictcmp(str, "env") || ft_strictcmp(str, "\'env\'")
-		|| ft_strictcmp(str, "\"env\""))
-		return (1);
-	if (ft_strictcmp(str, "exit") || ft_strictcmp(str, "\'exit\'")
-		|| ft_strictcmp(str, "\"exit\""))
-		return (1);
+	if (ft_strictcmp(str, "echo"))
+		return (ECHO);
+	if (ft_strictcmp(str, "cd"))
+		return (CD);
+	if (ft_strictcmp(str, "pwd"))
+		return (PWD);
+	if (ft_strictcmp(str, "export"))
+		return (EXPORT);
+	if (ft_strictcmp(str, "unset"))
+		return (UNSET);
+	if (ft_strictcmp(str, "env"))
+		return (ENV);
+	if (ft_strictcmp(str, "exit"))
+		return (EXIT);
 	return (0);
 }
